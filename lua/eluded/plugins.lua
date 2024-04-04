2fb51c1a0d757989bd065466b191c63f20d58cbe
Benjamin Gorman <8076bgorman@gmail.com>
2024-04-03 23:31:42 -0600

    Auto run PackerCompile when plugins file is saved

--- lua/eluded/plugins.lua
+++ lua/eluded/plugins.lua
@@ -1,3 +1,11 @@
+-- Automatically run PackerCompile when file is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   -- https://github.com/wbthomason/packer.nvim
